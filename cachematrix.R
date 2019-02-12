9deef4093317745d5136f9834b1fd579ee6b51db
Alisa <42485019+arcinfotech@users.noreply.github.com>
2019-02-11 17:10:17 -0700

    programming assignment 2
    contains 2 functions: makeCacheMatrix2 and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix2<-function(x = matrix()) {
+  mrows <- nrow(in_m)
+  mcols <- ncol(in_m)
+  im_vec <- c(x)
+  vlen <- length(im_vec)
+#create inverse of input matrix
+  inverse_v <- im_vec * t(im_vec)
+  i_matrix <- matrix(data = inverse_v, nrow = mrows, ncol = mcols)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#cache input matrix
+  o_matrix <<- i_matrix
+  return(o_matrix)
 }
 
 
-## Write a short comment describing this function
 
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
+##  if the input is the same (inverse) then return the cached matrix else return the inverse
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  mrows <- nrow(x)
+  mcols <- ncol(x)
+i_matrix<- matrix(data = NA, nrow = mrows, ncol = mcols)
+i_matrix <- NULL
+
+  if (is.matrix(x) ) {
+   cm <- makeCacheMatrix2(x)
+   v_cm <- c(cm)
+   v_x <- c(x)
+
+    if (!isTRUE(all.equal(v_x,v_cm))) {    
+      i_matrix <- makeCacheMatrix2(v_x)
+    }
+    else 
+      i_matrix<- o_matrix   
+  }
+  ##return inverse matrix
+  return(i_matrix)
 }
